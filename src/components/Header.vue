bd1677036bdf5b8774bf10032ba40489b52fb782
Elementrix <markijan8890.mg@gmail.com>
2024-10-04 17:26:17 +0300

    aded header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    <div class="title">Hub Cockpit</div>
+    <button class="add-button">+ New</button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-self: center;
+  padding: 10px;
+  margin-bottom: 10px;
+  /* border-radius: 10px; */
+}
+</style>
