ab0eb162f88661b70c1a7ba25858c3584ad83470
Elementrix <markijan8890.mg@gmail.com>
2024-10-04 20:04:58 +0300

    added search bar component with styles, inbox component without styles and change styles for index.html + header

---
+++ src/components/InboxComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="section">
+        <div class="section-title">
+            INBOX
+        </div>
+        <div class="messages">
+            <div>
+                <span>All unseen messages</span>
+                <span class="message-count">14</span>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InboxCoomponent'
+}
+</script>
